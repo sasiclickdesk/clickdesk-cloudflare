1fa3a0098d024e1624d37c4930b232868cd774f7
sasi krishna <sasi@faxdesk.com>
2014-04-11 11:49:48 +0530

    Initial commit with all files

---
+++ cloudflare.md
@@ -0,0 +1,5 @@
+#ClickDesk
+
+This app lets you complete the installation process of ClickDesk without copying the JS to code to every page in your website.
+
+You turn it on, and provide your ClickDesk WidgetID to complete the app installation process.
---
+++ public/javascripts/clickdesk.js
@@ -0,0 +1,25 @@
+var _glc;
+var glcp;
+var glcspt;
+var glcpath;
+CloudFlare.define(
+    'clickdesk', ['clickdesk/config'],
+    function (config) {
+
+	try{
+        console.log(config);
+		console.log(config.id);
+		
+		_glc =_glc || []; _glc.push(config.id);
+		glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' : 
+			'http://my.clickdesk.com/clickdesk-ui/browser/');
+		glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
+		glcspt = document.createElement('script'); glcspt.type = 'text/javascript'; 
+			glcspt.async = true; glcspt.src = glcpath + 'livechat-new-unminified.js';
+		var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
+	}
+	catch(err){
+	console.log(err);
+	}
+    }
+);
